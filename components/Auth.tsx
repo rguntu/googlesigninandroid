c55aef1a89c03971bf639a0e20b8a5f57799df5a
Raveendra Guntupalli <rguntupalli@liveperson.com>
2023-12-30 19:19:22 -0800

    first commit

---
+++ components/Auth.tsx
@@ -0,0 +1,50 @@
+import {
+    GoogleSignin,
+    GoogleSigninButton,
+    statusCodes,
+  } from '@react-native-google-signin/google-signin';
+import { supabase } from '../utils/soupabase';
+// androidid 1026427538181-2diipj0sbmeq66qht1j3c7utp99346f1.apps.googleusercontent.com
+//webid 1026427538181-2q54pideh6brm0smninclk1tek62dhmm.apps.googleusercontent.com
+  export default function() {
+    GoogleSignin.configure({
+        scopes: ['https://www.googleapis.com/auth/drive.readonly'], // what API you want to access on behalf of the user, default is email and profile
+        webClientId: '1026427538181-2q54pideh6brm0smninclk1tek62dhmm.apps.googleusercontent.com', // client ID of type WEB for your server (needed to verify user ID and offline access)
+        });
+
+    return(
+        <GoogleSigninButton
+            size={GoogleSigninButton.Size.Wide}
+            color={GoogleSigninButton.Color.Dark}
+            onPress={async () => {
+                try {
+                  await GoogleSignin.hasPlayServices();
+                  const userInfo = await GoogleSignin.signIn();
+                  //setState({ userInfo });
+                  console.log(JSON.stringify(userInfo, null, 2))
+                  if(userInfo.idToken) {
+                    const {data, error} = await supabase.auth.signInWithIdToken({
+                      provider: "google",
+                      token: userInfo.idToken})
+                      console.log(error, data)
+                  } else {
+                    throw new Error("no ID present")
+                    
+                  }
+                  
+                  
+                } catch (error: any) {
+                  if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+                    // user cancelled the login flow
+                  } else if (error.code === statusCodes.IN_PROGRESS) {
+                    // operation (e.g. sign in) is in progress already
+                  } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+                    // play services not available or outdated
+                  } else {
+                    // some other error happened
+                  }
+                }
+              }}
+        />
+    )
+  }
